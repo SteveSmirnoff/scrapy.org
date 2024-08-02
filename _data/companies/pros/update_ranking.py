ff08e888cba5433b4dbc73bebe11171214561bb0
Steve Smirnoff <stephen.smirnov1998@gmail.com>
2024-08-02 23:09:35 +0300

    set a newline explicitly

--- _data/companies/pros/update_ranking.py
+++ _data/companies/pros/update_ranking.py
@@ -4,19 +4,19 @@ from operator import itemgetter
 
 MIN_SCORE = 5
 
-with open('points.csv') as f:
+with open('points.csv', newline='') as f:
     reader = DictReader(f)
     catpoints = {r['category']: int(r['points']) for r in reader}
 
 points = defaultdict(int)
-with open('contributions.csv') as f:
+with open('contributions.csv', newline='') as f:
     reader = DictReader(f)
     for r in reader:
         points[r['company']] += catpoints[r['category']]
 
 ranking = sorted(points.items(), key=itemgetter(1), reverse=True)
 
-with open('ranking.csv', 'w') as f:
+with open('ranking.csv', 'w', newline='') as f:
     writer = DictWriter(f, ['company', 'score'])
     writer.writeheader()
     writer.writerows({'company': c, 'score': s}
